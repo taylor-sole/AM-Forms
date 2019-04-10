74cc17acf77d4bb1d098c1d0e1aa73927f47c676
MacBook Pro Tgriffith <macbookprotgriffith@sfpclocals-MacBook-Pro.local>
2019-04-09 17:57:31 -0700

    fix sort toggle

--- src/components/AmLeaderboard/am-leaderboard.js
+++ src/components/AmLeaderboard/am-leaderboard.js
@@ -11,7 +11,7 @@ class AmDashboard extends Component {
     super(props);
     this.state = {
       leaderboardList: null,
-      sortByValue: null
+      sortByValue: 'alphabetical'
     }
     this.sortLeaderboard = this.sortLeaderboard.bind(this);
   }
